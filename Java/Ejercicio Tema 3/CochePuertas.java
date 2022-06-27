28f2e0a7385c5744282fd789b9b867d9267856b1
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2022-06-27 20:37:56 -0300

    Ejercicio 3

---
+++ Java/Ejercicio Tema 3/CochePuertas.java
@@ -0,0 +1,23 @@
+public class CochePuertas {
+    public static void main(String[] argss) {
+    Coche miCoche = new Coche();
+    miCoche.AñadirPuerta();
+    miCoche.AñadirPuerta();
+    miCoche.AñadirPuerta();
+    miCoche.AñadirPuerta();
+    System.out.println(miCoche.puertas);
+    }
+        
+    public static int suma(int a, int b) {
+        return a + b;
+    }
+    
+}
+
+class Coche {
+    public int puertas = 0;
+
+    public void AñadirPuerta() {
+        this.puertas++;
+    }
+}
---
+++ Java/Ejercicio Tema 3/Suma.java
@@ -0,0 +1,17 @@
+
+public class Suma {
+
+    public static void main(String[] args) {
+        int n1 = 10;
+        int n2 = 20;
+        int n3 = 30;
+
+        suma(n1, n2, n3);
+    }
+
+    public static void suma(int a, int b, int c) {
+        System.out.println(a + b + c);
+    }
+}
+
+
